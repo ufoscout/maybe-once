6e3c87b93f5b74e64d4ccd7fcb2ed116f2d296e1
ufoscout <ufoscout@gmail.com>
2019-03-17 21:45:44 +0100

    First commint

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::SeqCst;
+
+pub struct MaybeSingle<T> {
+    data: Arc<Mutex<Option<T>>>,
+    init: fn() -> T,
+    callers: Arc<Mutex<AtomicUsize>>
+}
+
+impl <T> MaybeSingle<T> {
+
+    pub fn new(init: fn() -> T) -> Self {
+        MaybeSingle {
+            data: Arc::new(Mutex::new(None)),
+            init,
+            callers: Arc::new(Mutex::new(AtomicUsize::new(0)))
+        }
+    }
+
+    pub fn get(&self, callback: fn(data: &T)) {
+        {
+            let lock= self.callers.lock().unwrap();
+            let callers = lock.load(SeqCst) + 1;
+            lock.store(callers, SeqCst);
+        }
+        {
+            let mut lock = self.data.lock().unwrap();
+
+            if lock.is_none() {
+                *lock = Some((self.init)());
+            }
+
+            match lock.as_ref() {
+                Some(data) => callback(data),
+                None => panic!("There should always be data here!")
+            };
+        }
+        {
+            let lock= self.callers.lock().unwrap();
+            let callers = lock.load(SeqCst) - 1;
+            lock.store(callers, SeqCst);
+
+            if callers == 0 {
+                let mut data = self.data.lock().unwrap();
+                *data = None;
+            }
+        }
+
+    }
+}
