ee54a0736a6716c5d46d42819ded1e015f928606
ufoscout <ufoscout@gmail.com>
2019-07-07 22:15:44 +0200

    Free resources after panic

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::sync::{Arc, Mutex, RwLock};
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::SeqCst;
+use std::ops::Deref;
 
 pub struct MaybeSingle<T> {
-    data: Arc<RwLock<Option<T>>>,
+    data: Arc<RwLock<Option<Arc<T>>>>,
     init: fn() -> T,
     callers: Arc<Mutex<AtomicUsize>>
 }
@@ -29,27 +30,39 @@ impl <T> MaybeSingle<T> {
             let callers = lock.load(SeqCst) + 1;
             lock.store(callers, SeqCst);
         }
-        {
-            let mut lock = self.data.write().unwrap();
+        let data_arc = {
+            let mut lock = self.data.read().unwrap();
 
             lock = if lock.is_none() {
                 drop(lock);
                 {
                     let mut write_lock = self.data.write().unwrap();
-                    *write_lock = Some((self.init)());
+                    if write_lock.is_none() {
+                      //  println!("--- INIT ---");
+                        *write_lock = Some(Arc::new((self.init)()));
+                    }
                 }
-                self.data.write().unwrap()
+                self.data.read().unwrap()
             } else {
                 lock
             };
 
             //println!("---- Exec {}", rnd);
             match lock.as_ref() {
-                Some(data) => callback(data),
+                Some(data) => {
+                    data.clone()
+                },
                 None => panic!("There should always be data here!")
-            };
-        }
+            }
+        };
+        let data_wrap = Data{
+            data_arc,
+            data: self.data.clone(),
+            callers: self.callers.clone()
+        };
+        callback(&data_wrap);
         {
+            /*
             let lock= self.callers.lock().unwrap();
             let callers = lock.load(SeqCst) - 1;
             lock.store(callers, SeqCst);
@@ -58,8 +71,37 @@ impl <T> MaybeSingle<T> {
                 let mut data = self.data.write().unwrap();
                 *data = None;
             }
+            */
         }
         //println!("---- End {}", rnd);
 
+    }
+}
+
+pub struct Data<T> {
+    data_arc: Arc<T>,
+    data: Arc<RwLock<Option<Arc<T>>>>,
+    callers: Arc<Mutex<AtomicUsize>>
+}
+
+impl <T> Drop for Data<T> {
+    fn drop(&mut self) {
+        //println!("--- Dropping DATA ---");
+        let lock= self.callers.lock().unwrap();
+        let callers = lock.load(SeqCst) - 1;
+        lock.store(callers, SeqCst);
+
+        if callers == 0 {
+            let mut data = self.data.write().unwrap();
+            *data = None;
+        }
+    }
+}
+
+impl <T> Deref for Data<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.data_arc.as_ref()
     }
 }
