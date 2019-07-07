8aa35bdac1c840cd3e9c4def2d7448fc1d88a575
ufoscout <ufoscout@gmail.com>
2019-07-07 15:32:49 +0200

    Allow concurrent gets

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::SeqCst;
 
 pub struct MaybeSingle<T> {
-    data: Arc<Mutex<Option<T>>>,
+    data: Arc<RwLock<Option<T>>>,
     init: fn() -> T,
     callers: Arc<Mutex<AtomicUsize>>
 }
@@ -12,25 +12,38 @@ impl <T> MaybeSingle<T> {
 
     pub fn new(init: fn() -> T) -> Self {
         MaybeSingle {
-            data: Arc::new(Mutex::new(None)),
+            data: Arc::new(RwLock::new(None)),
             init,
             callers: Arc::new(Mutex::new(AtomicUsize::new(0)))
         }
     }
 
     pub fn get<F: FnOnce(&T)>(&self, callback: F) {
+
+        //let rnd: u16 = rand::thread_rng().gen();
+
+        //println!("---- Start {}", rnd);
+
         {
             let lock= self.callers.lock().unwrap();
             let callers = lock.load(SeqCst) + 1;
             lock.store(callers, SeqCst);
         }
         {
-            let mut lock = self.data.lock().unwrap();
+            let mut lock = self.data.write().unwrap();
 
-            if lock.is_none() {
-                *lock = Some((self.init)());
-            }
+            lock = if lock.is_none() {
+                drop(lock);
+                {
+                    let mut write_lock = self.data.write().unwrap();
+                    *write_lock = Some((self.init)());
+                }
+                self.data.write().unwrap()
+            } else {
+                lock
+            };
 
+            //println!("---- Exec {}", rnd);
             match lock.as_ref() {
                 Some(data) => callback(data),
                 None => panic!("There should always be data here!")
@@ -42,10 +55,11 @@ impl <T> MaybeSingle<T> {
             lock.store(callers, SeqCst);
 
             if callers == 0 {
-                let mut data = self.data.lock().unwrap();
+                let mut data = self.data.write().unwrap();
                 *data = None;
             }
         }
+        //println!("---- End {}", rnd);
 
     }
 }
