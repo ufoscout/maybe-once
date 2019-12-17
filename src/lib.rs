1f5251f61ff16fc73fb5a9721094504fb8165acb
ufoscout <ufoscout@gmail.com>
2019-12-17 21:30:53 +0100

    Can arc avoid poisoning?!

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,32 @@
-use std::sync::{Arc, Mutex, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::ops::Deref;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::SeqCst;
-use std::ops::Deref;
+use std::sync::{Arc, Mutex, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 pub struct MaybeSingle<T> {
     data: Arc<RwLock<Option<Arc<T>>>>,
     lock_mutex: Arc<RwLock<()>>,
     init: fn() -> T,
-    callers: Arc<Mutex<AtomicUsize>>
+    callers: Arc<Mutex<AtomicUsize>>,
 }
 
-impl <T> MaybeSingle<T> {
-
+impl<T> MaybeSingle<T> {
     pub fn new(init: fn() -> T) -> Self {
         MaybeSingle {
             data: Arc::new(RwLock::new(None)),
             init,
             lock_mutex: Arc::new(RwLock::new(())),
-            callers: Arc::new(Mutex::new(AtomicUsize::new(0)))
+            callers: Arc::new(Mutex::new(AtomicUsize::new(0))),
         }
     }
 
     pub fn get<F: FnOnce(&T)>(&self, no_parallel: bool, callback: F) {
-
         //let rnd: u16 = rand::thread_rng().gen();
 
         //println!("---- Start {}", rnd);
 
         {
-            let lock= self.callers.lock().unwrap();
+            let lock = self.callers.lock().unwrap();
             let callers = lock.load(SeqCst) + 1;
             lock.store(callers, SeqCst);
         }
@@ -40,7 +38,7 @@ impl <T> MaybeSingle<T> {
                 {
                     let mut write_lock = self.data.write().unwrap();
                     if write_lock.is_none() {
-                      //  println!("--- INIT ---");
+                        //  println!("--- INIT ---");
                         *write_lock = Some(Arc::new((self.init)()));
                     }
                 }
@@ -51,25 +49,29 @@ impl <T> MaybeSingle<T> {
 
             //println!("---- Exec {}", rnd);
             match lock.as_ref() {
-                Some(data) => {
-                    data.clone()
-                },
-                None => panic!("There should always be data here!")
+                Some(data) => data.clone(),
+                None => panic!("There should always be data here!"),
             }
         };
 
         let (read_lock, write_lock) = if no_parallel {
-            (None, Some(self.lock_mutex.write().unwrap()))
+            (
+                Arc::new(None),
+                Arc::new(Some(self.lock_mutex.write().unwrap())),
+            )
         } else {
-            (Some(self.lock_mutex.read().unwrap()), None)
+            (
+                Arc::new(Some(self.lock_mutex.read().unwrap())),
+                Arc::new(None),
+            )
         };
 
-        let data_wrap = Data{
+        let data_wrap = Data {
             data_arc,
             data: self.data.clone(),
             callers: self.callers.clone(),
             read_lock,
-            write_lock
+            write_lock,
         };
         callback(&data_wrap);
         {
@@ -85,22 +87,21 @@ impl <T> MaybeSingle<T> {
             */
         }
         //println!("---- End {}", rnd);
-
     }
 }
 
 pub struct Data<'a, T> {
     data_arc: Arc<T>,
     data: Arc<RwLock<Option<Arc<T>>>>,
-    read_lock: Option<RwLockReadGuard<'a, ()>>,
-    write_lock: Option<RwLockWriteGuard<'a, ()>>,
-    callers: Arc<Mutex<AtomicUsize>>
+    read_lock: Arc<Option<RwLockReadGuard<'a, ()>>>,
+    write_lock: Arc<Option<RwLockWriteGuard<'a, ()>>>,
+    callers: Arc<Mutex<AtomicUsize>>,
 }
 
-impl <'a, T> Drop for Data<'a, T> {
+impl<'a, T> Drop for Data<'a, T> {
     fn drop(&mut self) {
         //println!("--- Dropping DATA ---");
-        let lock= self.callers.lock().unwrap();
+        let lock = self.callers.lock().unwrap();
         let callers = lock.load(SeqCst) - 1;
         lock.store(callers, SeqCst);
 
@@ -111,7 +112,7 @@ impl <'a, T> Drop for Data<'a, T> {
     }
 }
 
-impl <'a, T> Deref for Data<'a, T> {
+impl<'a, T> Deref for Data<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -123,9 +124,9 @@ impl <'a, T> Deref for Data<'a, T> {
 mod test {
 
     use super::*;
+    use rand::{thread_rng, Rng};
     use std::thread::sleep;
     use std::time::Duration;
-    use rand::{thread_rng, Rng};
 
     #[test]
     fn should_execute_in_parallel() {
@@ -135,12 +136,13 @@ mod test {
 
         for i in 0..100 {
             let maybe = maybe.clone();
-            handles.push(std::thread::spawn(move ||
-            maybe.get(false, |_| {
-                println!(" exec {} start", i);
-                sleep(Duration::from_nanos(thread_rng().gen_range(0, 1000)));
-                println!(" exec {} end", i);
-            })));
+            handles.push(std::thread::spawn(move || {
+                maybe.get(false, |_| {
+                    println!(" exec {} start", i);
+                    sleep(Duration::from_nanos(thread_rng().gen_range(0, 1000)));
+                    println!(" exec {} end", i);
+                })
+            }));
         }
 
         for handle in handles {
@@ -156,12 +158,13 @@ mod test {
 
         for i in 0..100 {
             let maybe = maybe.clone();
-            handles.push(std::thread::spawn(move ||
+            handles.push(std::thread::spawn(move || {
                 maybe.get(true, |_| {
                     println!(" exec {} start", i);
                     sleep(Duration::from_nanos(thread_rng().gen_range(0, 1000)));
                     println!(" exec {} end", i);
-                })));
+                })
+            }));
         }
 
         for handle in handles {
