9d8d4abd1690da89c1a11ac9c8dd6208787dde1c
Francesco <ufoscout@gmail.com>
2025-04-12 09:48:27 +0200

    Simplify examples

--- examples/testcontainers/src/postgres_async.rs
+++ examples/testcontainers/src/postgres_async.rs
@@ -4,11 +4,14 @@ mod tests {
     
     use std::sync::OnceLock;
 
+    use deadpool::{managed::Pool, Runtime};
+    use deadpool_postgres::Manager;
     use maybe_once::tokio::{Data, MaybeOnceAsync};
+    use postgres::NoTls;
     use testcontainers::{runners::AsyncRunner, ContainerAsync};
     use testcontainers_modules::postgres::Postgres;
 
-    type MaybeOnceType = (String, ContainerAsync<Postgres>);
+    type MaybeOnceType = (Pool<Manager>, ContainerAsync<Postgres>);
 
     /// Initializer function.
     /// Starts a Postgres container shared between all tests.
@@ -17,13 +20,18 @@ mod tests {
         // startup the container
         let node = Postgres::default().start().await.expect("Could not start container");
 
-        // prepare connection string
-        let connection_string = format!(
-            "postgres://postgres:postgres@127.0.0.1:{}/postgres",
-            node.get_host_port_ipv4(5432).await.expect("Could not get db port")
-        );
+        let config = deadpool_postgres::Config {
+            user: Some("postgres".to_owned()),
+            password: Some("postgres".to_owned()),
+            dbname: Some("postgres".to_owned()),
+            host: Some("127.0.0.1".to_string()),
+            port: Some(node.get_host_port_ipv4(5432).await.unwrap()),
+            ..Default::default()
+        };
+   
+        let pool = config.create_pool(Some(Runtime::Tokio1), NoTls).expect("Could not create connection pool");
 
-        (connection_string, node)
+        (pool, node)
     }
     
     /// A function that holds a static reference to the container
@@ -41,62 +49,31 @@ mod tests {
     #[tokio::test]
     async fn test_1() {
         let data = data(false).await;
-        let connection_string = &data.0;
-
-        // container is up, you can use it
-        let (conn, connection) = tokio_postgres::connect(connection_string, tokio_postgres::NoTls).await.unwrap();
-
-            // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
+        let pool = &data.0;
+        let conn = pool.get().await.unwrap();
 
         let rows = conn.query("SELECT 1 + 1", &[]).await.unwrap();
         assert_eq!(rows.len(), 1);
     }
-
 
     #[tokio::test]
     async fn test_2() {
         let data = data(false).await;
-        let connection_string = &data.0;
-
-        // container is up, you can use it
-        let (conn, connection) = tokio_postgres::connect(connection_string, tokio_postgres::NoTls).await.unwrap();
-
-            // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
+        let pool = &data.0;
+        let conn = pool.get().await.unwrap();
 
         let rows = conn.query("SELECT 1 + 1", &[]).await.unwrap();
         assert_eq!(rows.len(), 1);
     }
-
 
     #[tokio::test]
     async fn test_3() {
         let data = data(false).await;
-        let connection_string = &data.0;
-
-        // container is up, you can use it
-        let (conn, connection) = tokio_postgres::connect(connection_string, tokio_postgres::NoTls).await.unwrap();
-
-            // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
+        let pool = &data.0;
+        let conn = pool.get().await.unwrap();
 
         let rows = conn.query("SELECT 1 + 1", &[]).await.unwrap();
         assert_eq!(rows.len(), 1);
     }
+
 }
